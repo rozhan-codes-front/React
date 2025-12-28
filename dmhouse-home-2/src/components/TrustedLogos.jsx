ae043f35ef0a16bf6a841a343b8bc5720d6450c5
Rozhan <rozhanboroumand00@gmail.com>
2025-12-28 16:09:07 +0330

    3d react

--- dmhouse-home-2/src/components/TrustedLogos.jsx
+++ dmhouse-home-2/src/components/TrustedLogos.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 
+// 1. Define your logos once
+const RAW_LOGOS = [
+    "https://haghanigold.com/wp-content/uploads/2024/11/cropped-haghani-logo.png",
+    "https://dmhouse.agency/wp-content/uploads/2022/10/JBH.jpg",
+    "https://dmhouse.agency/wp-content/uploads/2024/03/logo-14-min.png",
+    "https://dmhouse.agency/wp-content/uploads/2024/01/cropped-ieltsmix-fav-icon.png",
+    "https://dmhouse.agency/wp-content/uploads/2023/05/%D8%A7%D8%B1%D8%B4%D8%AF-%D9%BE%DB%8C.jpg"
+];
+
+// 2. Create a "Batch" that is definitely wider than any screen (repeat 4 times)
+const LOGO_BATCH = [...RAW_LOGOS, ...RAW_LOGOS, ...RAW_LOGOS, ...RAW_LOGOS];
+
 export default function TrustedLogos() {
+
+    // Helper to render the items
+    const renderLogoTrack = () => (
+        <>
+            {LOGO_BATCH.map((src, index) => (
+                <div className="logoItem" key={`set1-${index}`}>
+                    <img src={src} alt="Brand Logo" />
+                </div>
+            ))}
+            {LOGO_BATCH.map((src, index) => (
+                <div className="logoItem" key={`set2-${index}`}>
+                    <img src={src} alt="Brand Logo" />
+                </div>
+            ))}
+        </>
+    );
+
     return (
         <section className="trustedLogos">
             <header className="servicesStack__header">
@@ -11,42 +40,23 @@ export default function TrustedLogos() {
                     برندهایی که به ما اعتماد کرده اند
                 </p>
             </header>
+
             <div className="logoStage">
                 <div className="logoStage__center">
                     <img src="https://dmhouse.agency/wp-content/uploads/2024/08/team1-min-min.png" alt="DMHouse" />
                 </div>
 
-                {/* Marquee LTR */}
-                <div className="logoMarquee logoMarquee--ltr" dir="ltr">
+                {/* Row 1: Moves Left */}
+                <div className="logoMarquee" dir="ltr">
                     <div className="logoMarquee__track">
-                        <div className="logoItem"><img src="https://haghanigold.com/wp-content/uploads/2024/11/cropped-haghani-logo.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2022/10/JBH.jpg" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/03/logo-14-min.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/01/cropped-ieltsmix-fav-icon.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2023/05/%D8%A7%D8%B1%D8%B4%D8%AF-%D9%BE%DB%8C.jpg" alt="" /></div>
-                        {/* Duplicates for loop */}
-                        <div className="logoItem"><img src="https://haghanigold.com/wp-content/uploads/2024/11/cropped-haghani-logo.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2022/10/JBH.jpg" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/03/logo-14-min.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/01/cropped-ieltsmix-fav-icon.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2023/05/%D8%A7%D8%B1%D8%B4%D8%AF-%D9%BE%DB%8C.jpg" alt="" /></div>
+                        {renderLogoTrack()}
                     </div>
                 </div>
 
-                {/* Marquee RTL */}
-                <div className="logoMarquee logoMarquee--rtl">
+                {/* Row 2: Moves Right (Reverse) */}
+                <div className="logoMarquee logoMarquee--reverse" dir="ltr">
                     <div className="logoMarquee__track">
-                        <div className="logoItem"><img src="https://haghanigold.com/wp-content/uploads/2024/11/cropped-haghani-logo.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2022/10/JBH.jpg" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/03/logo-14-min.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/01/cropped-ieltsmix-fav-icon.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2023/05/%D8%A7%D8%B1%D8%B4%D8%AF-%D9%BE%DB%8C.jpg" alt="" /></div>
-                        {/* Duplicates */}
-                        <div className="logoItem"><img src="https://haghanigold.com/wp-content/uploads/2024/11/cropped-haghani-logo.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2022/10/JBH.jpg" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/03/logo-14-min.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2024/01/cropped-ieltsmix-fav-icon.png" alt="" /></div>
-                        <div className="logoItem"><img src="https://dmhouse.agency/wp-content/uploads/2023/05/%D8%A7%D8%B1%D8%B4%D8%AF-%D9%BE%DB%8C.jpg" alt="" /></div>
+                        {renderLogoTrack()}
                     </div>
                 </div>
             </div>
